aeb6d0fa355d70d6ef996aa36331bfe5fbb16136
Jasmine Smith <jasminesmit1@unomaha.edu>
2018-09-15 22:41:07 -0500

    final commit

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,4 +124,8 @@ STATIC_DIRS = 'static'
 STATICFILES_DIRS = [
     STATIC_DIRS,
 ]
-
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'your_account@gmail.com'
+EMAIL_HOST_PASSWORD = 'your_password'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
